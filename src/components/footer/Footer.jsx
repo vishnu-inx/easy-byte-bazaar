26807e63191b31d2d0f12fbec986f5c838df45b1
Vatsal Shah <vatsal.s@inheritx.com>
2024-01-22 11:19:12 +0530

    add year Footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,10 @@
 
 function Footer() {
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className="bg-gray-800 p-4 text-white text-center fixed w-full bottom-0">
-            <p>&copy; 2023 Store App. All rights reserved.</p>
+            <p>&copy; {currentYear} Store App. All rights reserved.</p>
         </footer>
     )
 }
